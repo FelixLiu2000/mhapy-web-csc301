146b9358914a1954f8a7746f294e2ed44c325ec3
Felix Liu <felixliu.liu@mail.utoronto.ca>
2021-03-08 18:42:40 -0500

    Fixed formatting errors

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 const log = console.log;
 
 // Express
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // Middleware
 // Parses HTTP JSON body into JS object
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + "/client/build"));
+app.use(express.static(__dirname + '/client/build'));
 // Serve index.html
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   // Serve index.html
-  res.sendFile(__dirname + "/client/build/index.html");
+  res.sendFile(__dirname + '/client/build/index.html');
 });
 
 const port = process.env.PORT || 4000;
