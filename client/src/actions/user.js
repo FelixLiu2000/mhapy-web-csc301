a0ea3f455b5833a7a196892e8d96b81252fed76d
Felix Liu <felixliu.liu@mail.utoronto.ca>
2021-04-23 03:57:50 -0400

    Implemented server websocket proxy & socket connections

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,3 +1,5 @@
+import io from "socket.io-client";
+
 // Send request to server to login a user
 export const login = async (user) => {
   const url = "/auth/login";
@@ -7,8 +9,8 @@ export const login = async (user) => {
     body: JSON.stringify(user),
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json",
-    },
+      "Content-Type": "application/json"
+    }
   });
   // Send request
   try {
@@ -30,4 +32,17 @@ export const login = async (user) => {
     console.error(error);
     return Promise.reject(error);
   }
+};
+
+// Connect to general websocket
+export const connectSocket = (uid) => {
+  const socket = io({ reconnection: false, transportOptions: { withCredentials: true } });
+  socket.on("connect", () => {
+    console.log("[SOCKET] Connected");
+    socket.emit("goOnline", uid);
+    console.log("[SOCKET] User Online");
+  });
+  socket.on("connect_error", () => {
+    console.error("[SOCKET] Connection refused");
+  });
 };
